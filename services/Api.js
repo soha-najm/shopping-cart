fb0a5940c71a90b3b69e0fd5821cf2f18eb10345
soha-najm <101249593+soha-najm@users.noreply.github.com>
2022-09-09 21:16:44 +0430

    Update Api.js

--- services/Api.js
+++ services/Api.js
@@ -3,11 +3,7 @@ import axios from "axios"
 
 const BASE_URL = "https://fakestoreapi.com"; 
 
-
-// baraye gereftane products
 const getProducts=async()=>{
-// bayad ta vaghti k req anjam nashode sabr kone va nare khate baed 
-// chon oonjuri response i vojud nadare k return kone
     const response = await axios.get(`${BASE_URL}/products`);
     return response.data;
 
