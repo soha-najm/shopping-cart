fe2d73db4cdc19419aa4e2a65ea8380d71eefb8b
soha-najm <101249593+soha-najm@users.noreply.github.com>
2022-09-09 21:13:44 +0430

    Update ProductContextProvider.js

--- Context/ProductContextProvider.js
+++ Context/ProductContextProvider.js
@@ -7,38 +7,20 @@ import { getProducts } from '../services/Api';
 export const ProductContext = createContext();
 
 
-
 const ProductContextProvider = ({children}) => {
-
-    // etelaate fetchAPI ro mirizim dakhele ye state
-     // va ye array ro b onvane vurudi midim 
-    // chon etelaati k gharare begirim yek array of object e:
+    
+  
         const [products , setProducts]= useState([]);
 
 
         useEffect(()=>{
-        // vaghti functioni darim k mikhaym dakhele useEffect 
-        // estefade konim behtare khode function ro dakhele 
-        // useEffect tarif konim jaye inke az birun biarimesh too 
-           
         const fetchAPI = async()=>{
-
              setProducts(await getProducts());
-        // chon darim az yek function async estefade mikone
-        // baraye estefade bayad oono dakhele function async benevisimesh.
-   
             }
             fetchAPI();
         }, [])
 
-
-        
-
-
     return (
-        // component hayi k mikhaym behesun context 
-        // pass bedim ro be shekle props az app.js miarim 
-        // inja va oona ro dakhele provider wrap mikonim 
         <ProductContext.Provider value={products} >
             {children}
         </ProductContext.Provider>
